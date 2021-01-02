4ce0be910b974d453efdb11e4636bf88e2b32cd6
BAKAOLC <2565128043@qq.com>
2021-01-02 19:50:58 +0800

    test

--- MPTimer/MpTimer.cs
+++ MPTimer/MpTimer.cs
@@ -39,40 +39,43 @@ namespace MPTimer
             }
 
             var pc = pluginInterface.ClientState.LocalPlayer;
-            if (blackMageOnly && pc.ClassJob.Id != 25) { return; }
-
-            updateTime(pc);
-            ImGui.SetNextWindowSizeConstraints(new Vector2(170, 10), new Vector2(float.MaxValue, float.MaxValue));
-
-            var windowFlag = ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar;
-            if (clickThrough)
+            if (pc != null)
             {
-                windowFlag = windowFlag | ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoMouseInputs;
-            }
+                if (blackMageOnly && pc.ClassJob.Id != 25) { return; }
 
-            if (ImGui.Begin("mp tick bar", windowFlag))
-            {
-                var width = 150;
-                var height = 8;
-                var windowPos = ImGui.GetWindowPos();
-                var basePos = new Vector2(windowPos.X + 5, windowPos.Y + 2);
+                updateTime(pc);
+                ImGui.SetNextWindowSizeConstraints(new Vector2(170, 10), new Vector2(float.MaxValue, float.MaxValue));
 
-                var timestamp = (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - mpTickTimestamp) % 3000;
-                ImGui.Text(mpTickTimestamp == 0 ? "Uninitialized" : String.Format("{0:N2}", 3 - timestamp / 1000.0));
+                var windowFlag = ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar;
+                if (clickThrough)
+                {
+                    windowFlag = windowFlag | ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoMouseInputs;
+                }
 
-                ImGui.GetWindowDrawList().AddRect(new Vector2(basePos.X - 1, basePos.Y - 1), new Vector2(basePos.X + width + 1, basePos.Y + height + 1), ImGui.GetColorU32(new Vector4(0.000f, 0.000f, 0.000f, 1.000f)), 0, ImDrawCornerFlags.None, 1);
+                if (ImGui.Begin("mp tick bar", windowFlag))
+                {
+                    var width = 150;
+                    var height = 8;
+                    var windowPos = ImGui.GetWindowPos();
+                    var basePos = new Vector2(windowPos.X + 5, windowPos.Y + 2);
 
-                var boundaryPosX = basePos.X + width * (3 - boundaryTime) / 3;
-                ImGui.GetWindowDrawList().AddRectFilled(basePos, new Vector2(boundaryPosX, basePos.Y + height), ImGui.GetColorU32(leftColor));
-                ImGui.GetWindowDrawList().AddRectFilled(new Vector2(boundaryPosX, basePos.Y), new Vector2(basePos.X + width, basePos.Y + height), ImGui.GetColorU32(rightColor));
+                    var timestamp = (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - mpTickTimestamp) % 3000;
+                    ImGui.Text(mpTickTimestamp == 0 ? "Uninitialized" : String.Format("{0:N2}", 3 - timestamp / 1000.0));
+
+                    ImGui.GetWindowDrawList().AddRect(new Vector2(basePos.X - 1, basePos.Y - 1), new Vector2(basePos.X + width + 1, basePos.Y + height + 1), ImGui.GetColorU32(new Vector4(0.000f, 0.000f, 0.000f, 1.000f)), 0, ImDrawCornerFlags.None, 1);
+
+                    var boundaryPosX = basePos.X + width * (3 - boundaryTime) / 3;
+                    ImGui.GetWindowDrawList().AddRectFilled(basePos, new Vector2(boundaryPosX, basePos.Y + height), ImGui.GetColorU32(leftColor));
+                    ImGui.GetWindowDrawList().AddRectFilled(new Vector2(boundaryPosX, basePos.Y), new Vector2(basePos.X + width, basePos.Y + height), ImGui.GetColorU32(rightColor));
 
 
-                var cursorPosXOffset = width * timestamp / (float)3000.0;
-                var cursorPosX = basePos.X + cursorPosXOffset;
-                ImGui.GetWindowDrawList().AddRectFilled(new Vector2(cursorPosX, windowPos.Y), new Vector2(cursorPosX + 2, windowPos.Y + 12), ImGui.GetColorU32(new Vector4(1.000f, 1.000f, 1.000f, 1.000f)));
+                    var cursorPosXOffset = width * timestamp / (float)3000.0;
+                    var cursorPosX = basePos.X + cursorPosXOffset;
+                    ImGui.GetWindowDrawList().AddRectFilled(new Vector2(cursorPosX, windowPos.Y), new Vector2(cursorPosX + 2, windowPos.Y + 12), ImGui.GetColorU32(new Vector4(1.000f, 1.000f, 1.000f, 1.000f)));
 
 
-                ImGui.End();
+                    ImGui.End();
+                }
             }
         }
 
